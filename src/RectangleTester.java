fbacdb05494bd87a18aeb9267f3ef51ef9e77bd8
Garrett Credi <gcc@ameritech.net>
2017-08-30 13:14:04 -0500

    Add choice for translation.

--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -10,6 +10,7 @@ public class RectangleTester {
         scan.nextLine();
         System.out.println("Please enter desired width");
         int width = scan.nextInt();
+        scan.nextLine();
         Rectangle rect = new Rectangle(0, 0, width, height);
         System.out.println(rect);
         double area = rect.getHeight() * rect.getWidth();
@@ -18,5 +19,19 @@ public class RectangleTester {
         double perimHand = 2 * (width + height);
         System.out.println("Rect area is " + area + ", calculated by hand is " + areaHand);
         System.out.println("Rect perim is " + perim + ", calculated by hand is " + perimHand);
+        System.out.println("Would you like to translate your box? [y for yes, n for no]");
+        String choice = scan.nextLine();
+        if(choice.equals("y")){
+            System.out.println("What is the x translation?");
+            int xTrans = scan.nextInt();
+            scan.nextLine();
+            System.out.println("What is the y translation?");
+            int yTrans = scan.nextInt();
+            scan.nextLine();
+            rect.translate(xTrans, yTrans);
+            System.out.println("Your new box is " + rect);
+        } else{
+            System.out.println("Ok... goodbye");
+        }
     }
 }
