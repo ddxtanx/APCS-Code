6564767f7a073ddd254ce3d086d6f315ffb3e126
Garrett Credi <gcc@ameritech.net>
2017-09-26 12:55:08 -0500

    Add InputTesting code, get's users name and outputs initials en capital.

--- src/chap34/InputTesting.java
+++ src/chap34/InputTesting.java
@@ -1,8 +1,35 @@
 package chap34;
 
+import java.util.Scanner;
+
 public class InputTesting {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = "";
+        while(firstName.equals("") || firstName.contains(" ")) {
+            System.out.println("Enter your first name: ");
+            firstName = scan.nextLine();
+        }
+        //firstName is not "" and does not contain spaces
+        String middleName = "";
+        while(middleName.equals("") || middleName.contains(" ")) {
+            System.out.println("Enter your middle name: ");
+            middleName = scan.nextLine();
+        }
+        //Same as firstName logic
+        String lastName = "";
+        while(lastName.equals("") || lastName.contains(" ")) {
+            System.out.println("Enter your last name: ");
+            lastName = scan.nextLine();
+        }
+        //Same as firstName logic
 
+        char[] initials = {firstName.charAt(0), middleName.charAt(0), lastName.charAt(0)};
+        //Create a char array with first letter of each name, form the initials
+        String initialsString = new String(initials);
+        //Initialize a new string that is the adjoinment of the char array
+        System.out.println("Your initials are " + initialsString.toUpperCase());
+        //Print out initials en capital.
     }
 }
